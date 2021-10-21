ec5ebe74cb5ba33146facb86f23c98a668e4018d
nithinramesh7 <88645956+nithinramesh7@users.noreply.github.com>
2021-10-21 09:21:37 +0530

    BookApp assignment

--- WPFworks/BookApp/Book.cs
+++ WPFworks/BookApp/Book.cs
@@ -4,17 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Wpf01Xaml
+namespace BookApp
 {
     public class Book
     {
         public string Title { get; set; }
         public string Author { get; set; }
-        public int Price { get; set; }
-        public override string ToString()
-        {
-            return string.Format("{0} by {1}", Title, Author);
-        }
 
+        public int Price { get; set; }
+        //public override string ToString()
+        //{
+        //    return string.Format("{0} by {1} (Price: Rs{2})", Title, Author, Price);
+        //}
     }
 }
---
+++ WPFworks/BookApp/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookApp
+{
+   
+    public partial class MainWindow : Window
+    {
+        List<Book> mybook = new List<Book>();
+        Book bk = new Book();
+        int i = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Clear()
+        {
+
+            txtAuthor.Text = "";
+            txtPrice.Text = "";
+            txtTitle.Text = "";
+            lblStatus.Content = "";
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (txtTitle.Text != "" && txtAuthor.Text != "" && txtPrice.Text != "")
+                {
+                    Book bookdetails = new Book();
+                    bookdetails.Title = txtTitle.Text;
+                    bookdetails.Author = txtAuthor.Text;
+                    bookdetails.Price = Convert.ToInt32(txtPrice.Text);
+                    mybook.Add(bookdetails);
+                    MessageBox.Show("BOOK Details are added Successfully", "Message", MessageBoxButton.OK, MessageBoxImage.Information);
+                    Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Please fill the Details Completely", "Warning", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                }
+
+                // lblStatus.Content = "1 of " + mybook.Count.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (bk != null)
+                {
+                    mybook.Remove(bk);
+                    MessageBox.Show("Book Details Deleted Successfully", "Message", MessageBoxButton.OK, MessageBoxImage.Information);
+                    Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Please select the Book Details", "Warning", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void btnFirst_Click(object sender, RoutedEventArgs e)
+        {
+            if (mybook.Count > 0)
+            {
+                i = 0;
+                bk = mybook[i];
+                txtTitle.Text = bk.Title.ToString();
+                txtAuthor.Text = bk.Author.ToString();
+                txtPrice.Text = bk.Price.ToString();
+                lblStatus.Content = (i + 1).ToString() + " of " + mybook.Count.ToString();
+            }
+        }
+        private void btnLast_Click(object sender, RoutedEventArgs e)
+        {
+            if (mybook.Count > 0)
+            {
+                i = mybook.Count - 1;
+                bk = mybook[i];
+                txtTitle.Text = bk.Title.ToString();
+                txtAuthor.Text = bk.Author.ToString();
+                txtPrice.Text = bk.Price.ToString();
+                lblStatus.Content = (i + 1).ToString() + " of " + mybook.Count.ToString();
+            }
+
+        }
+        private void btnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            if (mybook.Count > 0)
+            {
+                if (i == mybook.Count - 1 || i != 0)
+                {
+                    i--;
+                    bk = mybook[i];
+                    txtTitle.Text = bk.Title.ToString();
+                    txtAuthor.Text = bk.Author.ToString();
+                    txtPrice.Text = bk.Price.ToString();
+                    lblStatus.Content = (i + 1).ToString() + " of " + mybook.Count.ToString();
+
+                }
+            }
+        }
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            if (mybook.Count > 0)
+            {
+                if (i < mybook.Count - 1)
+                {
+                    i++;
+                    bk = mybook[i];
+                    txtTitle.Text = bk.Title.ToString();
+                    txtAuthor.Text = bk.Author.ToString();
+                    txtPrice.Text = bk.Price.ToString();
+
+                    lblStatus.Content = (i + 1).ToString() + " of " + mybook.Count.ToString();
+
+                }
+            }
+        }
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+
+            Clear();
+        }
+    }
+}
