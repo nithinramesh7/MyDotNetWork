d3ccacee38db5e660b944c0870ea208235829915
nithinramesh7 <88645956+nithinramesh7@users.noreply.github.com>
2021-10-22 08:52:17 +0530

    Added curencyWpf work

---
+++ WPFworks/ConceptArchitect.Wpf/Currency.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace ConceptArchitect.Wpf
+{
+    public class Currency : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return ToString();  //return this value to whoever ask.
+        }
+
+
+
+        public string currency;
+
+        public string Value
+        {
+            get
+            {
+                return currency.ToString();
+            }
+            set
+            {
+                currency = value;
+            }
+        }
+
+
+        public string Symbol { get; set; }
+
+        public Currency()
+        {
+
+            Symbol = "₹";
+        }
+        public Currency(string value)
+        {
+            Value = Symbol + value;
+        }
+
+
+        public override string ToString()
+        {
+            return Symbol + "" + currency.ToString();
+        }
+
+
+    }
+}
---
+++ WPFworks/ConceptArchitect1.Wpf/Currency.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace ConceptArchitect1.Wpf
+{
+    public class Currency : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return ToString();  //return this value to whoever ask.
+        }
+
+
+
+        public string currency;
+
+        public string itsvalue
+        {
+            get
+            {
+                return currency.ToString();
+            }
+            set
+            {
+                currency = itsvalue;
+            }
+        }
+
+
+        public string Symbol { get; set; }
+
+        public Currency()
+        {
+
+            Symbol = "₹";
+        }
+        public Currency(string itsvalue)
+        {
+            itsvalue = Symbol + itsvalue;
+        }
+
+
+        public override string ToString()
+        {
+            return Symbol + "" + currency.ToString();
+        }
+
+
+    }
+    
+}
